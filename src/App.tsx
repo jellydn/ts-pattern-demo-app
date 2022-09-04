ee28c6f6180d4399eeea994787b611d0e619da38
Huynh Duc Dung <dunghd.it@gmail.com>
2022-09-04 14:22:12 +0800

    :sparkles: :state reducer example with ts-pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,118 @@
-import React, { useState } from "react";
+import { useReducer } from "react";
+import { P, match } from "ts-pattern";
 
 import "./App.css";
 import logo from "./logo.svg";
 
+type ApiState =
+  | { status: "idle" }
+  | { status: "loading"; startTime: number }
+  | { status: "success"; data: string }
+  | { status: "error"; error: Error };
+
+type ApiEvent =
+  | { type: "fetch" }
+  | { type: "success"; data: string }
+  | { type: "error"; error: Error }
+  | { type: "cancel" };
+
+const reducer = (apiState: ApiState, apiEvent: ApiEvent): ApiState =>
+  match<[ApiState, ApiEvent], ApiState>([apiState, apiEvent])
+    .with([{ status: "loading" }, { type: "success" }], ([, event]) => ({
+      status: "success",
+      data: event.data,
+    }))
+    .with(
+      [{ status: "loading" }, { type: "error", error: P.select() }],
+      (error) => ({
+        status: "error",
+        error,
+      })
+    )
+    .with([{ status: P.not("loading") }, { type: "fetch" }], () => ({
+      status: "loading",
+      startTime: Date.now(),
+    }))
+    .with(
+      [
+        {
+          status: "loading",
+          startTime: P.when((t) => t + 500 < Date.now()),
+        },
+        { type: "cancel" },
+      ],
+      () => ({
+        status: "idle",
+      })
+    )
+    .with(P._, () => apiState)
+    .exhaustive();
+
+const fakeFetchApi = (): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const now = Date.now();
+    setTimeout(() => {
+      if (now % 2 === 0) {
+        resolve("Hello Ts Pattern");
+      } else {
+        reject("Oops! Something went wrong!");
+      }
+    }, 1000);
+  });
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    status: "idle",
+  });
+
+  const onClickHandler = async () => {
+    dispatch({ type: "fetch" });
+    try {
+      const data = await fakeFetchApi();
+      dispatch({ type: "success", data });
+    } catch (error) {
+      dispatch({ type: "error", error: error as Error });
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <p>Hello Ts-Pattern + React!</p>
       </header>
+      <main>
+        {match(state)
+          .with({ status: "loading" }, () => {
+            return <p>Loading...</p>;
+          })
+          .with({ status: "error" }, (event) => {
+            return (
+              <p>
+                Error
+                <pre>{JSON.stringify(event, null, 2)}</pre>
+              </p>
+            );
+          })
+          .with({ status: "success" }, (event) => {
+            return (
+              <p>
+                Success!
+                <pre>{JSON.stringify(event, null, 2)}</pre>
+              </p>
+            );
+          })
+          .with({ status: "idle" }, () => {
+            return <p>Press below button to start</p>;
+          })
+          .exhaustive()}
+        <div className="buttons">
+          <button onClick={onClickHandler}>Call Fetch Api</button>
+          <button onClick={() => dispatch({ type: "cancel" })}>
+            Cancel Api
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
